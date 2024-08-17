5fda97faf242520402b4a4e9e32bdf7b78cbc821
cdseaholm <cdseaholm@gmail.com>
2024-08-17 14:19:57 -0600

    8/17/24 googletagscripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import PageWrapper from "@/components/utility/pageWrapper";
 import { Metadata } from "next";
 import Script from 'next/script';
 
-const livvic = Livvic({subsets: ['latin'], weight: '400', style: 'normal'});
+const livvic = Livvic({ subsets: ['latin'], weight: '400', style: 'normal' });
 
-export const metadata: Metadata ={
+export const metadata: Metadata = {
   title: "New Progress Co",
   description: "New Progress Co is an Multi-Purpose Development Company that specializes in Web Development, Mobile Development, and App Development"
 }
@@ -19,10 +19,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={livvic.className}>
       <AnimateWrapper>
         <body>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-FYZSYN60M3" strategy="afterInteractive" id="googleTag" />
+          <Script strategy="afterInteractive" id="googleDataLayer">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FYZSYN60M3');
+            `}
+          </Script>
           <PageWrapper>
             {children}
           </PageWrapper>
-          <script src="../path/to/flowbite/dist/flowbite.min.js" defer/>
+          <script src="../path/to/flowbite/dist/flowbite.min.js" defer />
         </body>
       </AnimateWrapper>
     </html>
