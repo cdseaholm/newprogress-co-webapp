c508dea0e4eacd48cdedfda33bddc81ff742343f
cdseaholm <cdseaholm@gmail.com>
2024-12-07 15:36:00 -0700

    12/7/24

--- components/pagecomponents/services/pageNav/dropDownNav.tsx
+++ components/pagecomponents/services/pageNav/dropDownNav.tsx
@@ -1,67 +1,105 @@
 'use client'
 
 import { Dropdown, DropdownTrigger, Button, DropdownMenu, DropdownItem, DropdownSection } from "@nextui-org/react";
-import { FiMenu } from "react-icons/fi";
-import React from "react";
-import SocialButton from "@/components/buttons/socialIconButton";
+import { FiBookOpen, FiClipboard, FiDollarSign, FiMail, FiMenu, FiMic } from "react-icons/fi";
+import React, { useState } from "react";
 
-export default function DropDownNav({ open, setOpen, tabs, handleClickedTab, currentSelection, icons }: { open: boolean, setOpen: (open: boolean) => void, tabs: string[], handleClickedTab: (index: number) => void, currentSelection: number, icons: JSX.Element[] }) {
+export const ServicesTab = () => {
+    return (
+        <FiClipboard />
+    )
+}
 
-    const ref = React.useRef<HTMLDivElement>(null);
+export const PricingTab = () => {
+    return (
+        <FiDollarSign />
+    )
+}
 
-    React.useEffect(() => {
-        const handleOutsideClick = (event: { target: any }) => {
-            if (open && !ref.current?.contains(event.target as HTMLDivElement)) {
-                setOpen(false);
-            }
-        };
-        window.addEventListener('click', handleOutsideClick, true);
-        return () => window.removeEventListener('click', handleOutsideClick, true);
-    }, [ref, setOpen, open]);
+export const AboutTab = () => {
+    return (
+        <FiBookOpen />
+    )
+}
+
+export const TestimonialsTab = () => {
+    return (
+        <FiMic />
+    )
+}
+
+export const ContactTab = () => {
+    return (
+        <FiMail />
+    )
+}
+
+export default function DropDownNav({ handleClickedTab }: { handleClickedTab: (tab: string) => void }) {
+
+    {/**['Harbor', 'Financr', 'Trackr', 'Gamr'] */ }
+
+    const [currentTab, setCurrentTab] = useState<string>("Services");
 
     return (
-        <div ref={ref}>
-            <Dropdown backdrop="blur" isOpen={open}>
-                <DropdownTrigger>
-                    <Button variant='flat' className={`hover:underline bg-transparent font-medium px-0 min-w-4`} onClick={() => setOpen(!open)}>
-                        <div className="flex flex-row items-center justify-end space-x-3">
-                            <p className='font-semibold text-xs text-themeStone/80'>{tabs[currentSelection]}</p>
-                            <FiMenu size={21} />
-                        </div>
-                    </Button>
-                </DropdownTrigger>
-                <DropdownMenu variant="faded" aria-label="Static Actions" className="bg-stone-200 rounded-sm" closeOnSelect={true}>
-                    <DropdownSection className="text-black hover:underline px-1 rounded-md flex-wrap rounded-md">
-                        {tabs.map((tab, index) => (
-                            <DropdownItem key={index} value={tab} onClick={(e) => {
-                                e.preventDefault();
-                                handleClickedTab(index)
-                            }} textValue={tab}>
-                                <Button type="button" title={tab} variant='flat' className={`bg-transparent font-semi-bold text-base ${currentSelection === index ? 'underline font-bold text-themeStone hover:cursor-default' : ''}`} onClick={(e) => {
-                                    e.preventDefault();
-                                    handleClickedTab(index)
-                                }} disabled={currentSelection === index ? true : false}>
-                                    <div className="flex flex-row items-center justify-center space-x-2">
-                                        <div>
-                                            {icons[index]}
-                                        </div>
-                                        <div>
-                                            {tab}
-                                        </div>
-                                    </div>
-                                </Button>
-                            </DropdownItem>
-                        ))}
-                    </DropdownSection>
-                    <DropdownItem className="text-black hover:underline px-1 rounded-md flex-wrap rounded-md" textValue="contact">
-                        <div className="flex flex-row items-center justify-center space-x-2 text-sm md:text-base">
-                            <SocialButton networkName='linkedin' parent={true} />
-                            <div className='md:mx-1 mx-0'>|</div>
-                            <SocialButton networkName='github' parent={true} />
-                        </div>
+        <Dropdown backdrop="blur" className="w-full">
+            <DropdownTrigger>
+                <Button className={`hover:underline bg-transparent font-medium min-w-4 w-full capitalize`}>
+                    <div className="flex flex-row justify-end items-center w-full space-x-3">
+                        <p>{currentTab}</p>
+                        <FiMenu size={21} />
+                    </div>
+                </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+                variant="faded"
+                aria-label="Static Actions"
+                className="bg-stone-200 rounded-sm"
+                closeOnSelect={true}
+                onAction={(key) => {
+                    let stringKey = key.toString();
+                    let firstChar = stringKey.charAt(0).toUpperCase();
+                    let restOfString = stringKey.slice(1);
+                    let upperString = firstChar + restOfString;
+                    setCurrentTab(upperString);
+                    handleClickedTab(stringKey);
+                }}
+                selectionMode="single"
+                selectedKeys={currentTab}
+                disallowEmptySelection
+            >
+                <DropdownSection className="text-black text-md md:text-lg hover:underline px-1 rounded-md flex-wrap rounded-md">
+                    <DropdownItem
+                        key={'services'}
+                        startContent={<ServicesTab />}
+                    >
+                        Services
                     </DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
-        </div>
+                    <DropdownItem
+                        key={'pricing'}
+                        startContent={<PricingTab />}
+                    >
+                        Pricing
+                    </DropdownItem>
+                    <DropdownItem
+                        key={'about'}
+                        startContent={<AboutTab />}
+                    >
+                        About
+                    </DropdownItem>
+                    <DropdownItem
+                        key={'testimonials'}
+                        startContent={<TestimonialsTab />}
+                    >
+                        Testimonials
+                    </DropdownItem>
+                    <DropdownItem
+                        key={'contact'}
+                        startContent={<ContactTab />}
+                    >
+                        Contact
+                    </DropdownItem>
+                </DropdownSection>
+            </DropdownMenu>
+        </Dropdown>
     )
 }
--- components/pagecomponents/services/pricing.tsx
+++ components/pagecomponents/services/pricing.tsx
@@ -2,26 +2,43 @@
 
 import { Button } from "@nextui-org/react";
 import UpFrontPricing from "./pricing/upFrontPricing";
-import MonthlyPricing from "./pricing/monthlyPricing";
+import MonthlyPricing, { ProductType } from "./pricing/monthlyPricing";
+import { useModalStore } from "@/context/modalStore";
 
 export default function PricingComponent({ priceUpFront, handleMonthlyClick, handleUpFrontClick }: { priceUpFront: boolean, handleMonthlyClick: () => void, handleUpFrontClick: () => void }) {
 
+    const setInfo = useModalStore((state) => state.setInfo);
+    const setInfoModal = useModalStore((state) => state.setInfoModal);
+
+    const handleInfo = (prod: ProductType) => {
+        if (!prod) {
+            return;
+        }
+        setInfo({
+            title: prod.name,
+            price: prod.cost,
+            explanation: prod.explanation,
+            details: prod.details
+        });
+        setInfoModal(true);
+    }
+    
     return (
         <div className={`flex flex-col items-center justify-center w-full h-full pb-10`}>
             <div className="flex flex-row items-center justify-evenly self-center py-2">
-                <Button variant="flat" type="button" className={`${!priceUpFront ? 'bg-gray-300 text-themeStone' : 'hover:bg-themeAcqua text-themeWater hover:text-black bg-transparent'} font-semi-bold text-base md:text-lg`} onClick={handleMonthlyClick} disabled={!priceUpFront ? true : false}>
+                <Button variant="flat" type="button" className={`${!priceUpFront ? 'bg-gray-300 text-themeStone' : 'hover:bg-themeAcqua text-themeWater hover:text-black bg-transparent'} font-semi-bold text-base md:text-lg`} onPress={handleMonthlyClick} disabled={!priceUpFront ? true : false}>
                     Monthly Pricing
                 </Button>
-                <Button variant="flat" type="button" className={`${priceUpFront ? 'bg-gray-300 text-themeStone' : 'hover:bg-themeAcqua text-themeWater hover:text-black bg-transparent'} font-semi-bold text-base md:text-lg`} onClick={handleUpFrontClick} disabled={priceUpFront ? true : false}>
+                <Button variant="flat" type="button" className={`${priceUpFront ? 'bg-gray-300 text-themeStone' : 'hover:bg-themeAcqua text-themeWater hover:text-black bg-transparent'} font-semi-bold text-base md:text-lg`} onPress={handleUpFrontClick} disabled={priceUpFront ? true : false}>
                     Up Front Pricing
                 </Button>
             </div>
             <div className="relative w-full h-full overflow-hidden">
                 <div className={`absolute top-5 left-0 w-full h-full transition-transform duration-500 ease-in-out ${!priceUpFront ? 'transform translate-x-0' : 'transform -translate-x-full'}`}>
-                    <MonthlyPricing />
+                    <MonthlyPricing handleInfo={handleInfo}/>
                 </div>
                 <div className={`absolute top-5 left-0 w-full h-full transition-transform duration-500 ease-in-out ${!priceUpFront ? 'transform translate-x-full' : 'transform translate-x-0'}`}>
-                    <UpFrontPricing />
+                    <UpFrontPricing handleInfo={handleInfo}/>
                 </div>
             </div>
         </div>
